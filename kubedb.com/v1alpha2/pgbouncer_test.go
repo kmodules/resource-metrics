700d1fb225b884e9db0856ca2a0fa0d7b86855f7
Tamal Saha <tamal@appscode.com>
2022-10-01 13:22:30 -0700

    Use gomodules.xyz/testing (#32)
    Signed-off-by: Tamal Saha <tamal@appscode.com>
    
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- kubedb.com/v1alpha2/pgbouncer_test.go
+++ kubedb.com/v1alpha2/pgbouncer_test.go
@@ -19,9 +19,8 @@ package v1alpha2
 import (
 	"testing"
 
-	mt "kmodules.xyz/resource-metrics/testing"
-
 	"github.com/google/go-cmp/cmp"
+	tl "gomodules.xyz/testing"
 	core "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 )
@@ -67,7 +66,7 @@ func TestPgBouncer(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			obj, err := mt.Load(tt.name)
+			obj, err := tl.LoadFile(tt.name)
 			if err != nil {
 				t.Error(err)
 				return
