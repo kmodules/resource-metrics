cf0239f6464b24b1cfa2af2f7800a5f4ef860d1d
Arnob kumar saha <arnob@appscode.com>
2024-07-11 19:12:02 +0600

    master -> shards
    Signed-off-by: Arnob kumar saha <arnob@appscode.com>

--- kubedb.com/v1/redis.go
+++ kubedb.com/v1/redis.go
@@ -115,7 +115,7 @@ func (r Redis) roleResourceFn(fn func(rr core.ResourceRequirements) core.Resourc
 			return nil, err
 		}
 		if found && mode == DBModeCluster {
-			shards, _, err := unstructured.NestedInt64(obj, "spec", "cluster", "master")
+			shards, _, err := unstructured.NestedInt64(obj, "spec", "cluster", "shards")
 			if err != nil {
 				return nil, err
 			}
